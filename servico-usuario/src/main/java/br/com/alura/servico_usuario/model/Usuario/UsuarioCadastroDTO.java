e7a9b586b4b7912df508faa96f26f927dbec75d7
Marcelo <mdbezerra16@gmail.com>
2025-12-19 20:15:53 -0300

    feat: :sparkles: Impressão de log com dados da reserva ao agendar nova reserva

--- servico-usuario/src/main/java/br/com/alura/servico_usuario/model/Usuario/UsuarioCadastroDTO.java
+++ servico-usuario/src/main/java/br/com/alura/servico_usuario/model/Usuario/UsuarioCadastroDTO.java
@@ -2,6 +2,7 @@ package br.com.alura.servico_usuario.model.Usuario;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.hibernate.annotations.processing.Pattern;
 
 public record UsuarioCadastroDTO(
     @NotBlank String nome,
